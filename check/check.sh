e35323a05fcf023a5c93c64de6c8fce00093cbef
StarryWang <starry.wang@suse.com>
2024-10-31 16:16:15 +0800

    Update check script for 2.10

--- check/check.sh
+++ check/check.sh
@@ -8,6 +8,15 @@ RANCHER_VERSION="${RANCHER_VERSION:-}"
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 if [[ -z "$RANCHER_VERSION" ]]; then
     case $BRANCH_NAME in
+        */v2.12)
+            RANCHER_VERSION="v2.9"
+            ;;
+        */v2.11)
+            RANCHER_VERSION="v2.9"
+            ;;
+        */v2.10)
+            RANCHER_VERSION="v2.9"
+            ;;
         */v2.9)
             RANCHER_VERSION="v2.9"
             ;;
