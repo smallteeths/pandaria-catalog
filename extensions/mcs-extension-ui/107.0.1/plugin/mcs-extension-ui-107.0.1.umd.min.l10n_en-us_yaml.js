5dc46d1cf5965df1e8c9d8e4bd52bca7296ad64b
smallteeths <503630985@qq.com>
2025-11-11 16:31:08 +0800

    Add mcs-extension-ui chart

---
+++ extensions/mcs-extension-ui/107.0.1/plugin/mcs-extension-ui-107.0.1.umd.min.l10n_en-us_yaml.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+((typeof self !== 'undefined' ? self : this)["webpackChunkmcs_extension_ui_107_0_1"] = (typeof self !== 'undefined' ? self : this)["webpackChunkmcs_extension_ui_107_0_1"] || []).push([["l10n_en-us_yaml"],{
+
+/***/ "./l10n/en-us.yaml":
+/*!*************************!*\
+  !*** ./l10n/en-us.yaml ***!
+  \*************************/
+/***/ (function(module) {
+
+eval("const doc = [({\"mcs\":({\"nav\":({\"dashboard\":({\"label\":\"Dashboard\"}), \"clusterMove\":\"Move\", \"clusters\":\"Clusters\"}), \"clusterSets\":({\"label\":\"ClusterSets/Clusters\"}), \"welcome\":\"Welcome to Multi-Cluster Service\", \"chartNotInstallTips\":({\"global\":\"The mcs-addon-global-system is not installed\", \"cluster\":\"The mcs-addon-system is not installed\"}), \"install\":\"Install\", \"noPermission\":\"No right of access to relevant resources\", \"notClusterSetMemberTips\":\"The current cluster is not a member of a ClusterSet, please go to the ClusterSet page to set up the membership.\", \"chart\":({\"installType\":\"Installation Type\", \"submarinerClusterID\":\"Submariner ClusterID\", \"submarinerBrokerUrl\":\"Submariner Broker Url\", \"submarinerClusterCIDR\":\"Submariner Cluster CIDR\", \"submarinerServiceCIDR\":\"Submariner Service CIDR\"}), \"table\":({\"addCluster\":\"Add Cluster\", \"groupBy\":({\"name\":\"Group by Name\"}), \"noClusters\":\"There are no clusters added.\", \"notInClusterset\":\"Not in a clusterset\"}), \"addClustersetMembersModal\":({\"title\":\"Add Clusterset {name} Member\"}), \"moveClustersetMemeberModal\":({\"title\":\"Move to a new clusterset\", \"desc\":\"You are moving the following clusters:\", \"targetClusterset\":\"Target Clusterset\"}), \"clusterSelect\":({\"label\":\"Clusters\"}), \"errors\":({\"missingClusterId\":\"Failed to load provisioning clusters\", \"provNotFound\":\"Provisioning cluster not found\", \"fetchCacertsFail\":\"Unable to fetch cacerts\", \"fetchServerUrlFail\":\"Unable to fetch server-url\", \"cidrMissing\":\"Unable to get {path}\", \"brokerRegisterFail\":\"Broker registration failed\", \"clustersetNoMatch\":\"No ClusterSet matched for {name}\", \"pskFetchFail\":\"Fetch PSK Secret failed\", \"deleteNoClusterName\":\"Current cluster has no clusterName\", \"deleteFailed\":\"Delete Submariner failed\", \"deleteTimeout\":\"Deletion wait timed out (>{timeoutMs}ms)\", \"valuesReadFailed\":\"Failed to read installed Chart values\", \"appListFetchFailed\":\"Failed to fetch app list\", \"appNotFound\":\"Installed mcs-ext-chart not found\", \"secretAnnoMissing\":\"Missing Secret annotations containing values\", \"badReleaseName\":\"Invalid upgrade payload: missing releaseName\", \"badPsk\\\"\":\"Invalid upgrade parameters: unable to retrieve Secret PSK\", \"brokerFetchFailed\":\"Failed to fetch brokerData\", \"chartUpgradeFailed\":\"Chart upgrade failed\", \"upgradeFailed\":\"Upgrade failed\"}), \"progress\":({\"deletingCrs\":\"Deleting Submariner CRs\", \"waitingCleanup\":\"Deletion may take a few minutes. Waiting for Submariner CR cleanup…\", \"cleared\":\"Submariner CR cleanup done\", \"retrying\":\"Error during Submariner CR query, retrying…\"})}), \"product\":({\"multiClusterService\":\"Multi-Cluster Service\", \"globalMultiClusterService\":\"Multi-Cluster Service\"})})];\nmodule.exports = doc.length <= 1 ? doc[0] : doc;\n\n//# sourceURL=webpack://mcs-extension-ui-107.0.1/./l10n/en-us.yaml?");
+
+/***/ })
+
+}]);
